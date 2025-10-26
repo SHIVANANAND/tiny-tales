bfcd009eaa548871f148245342ba9f081711ea4c
Rudraksh-r <rudraksh.r6105@gmail.com>
2025-10-27 04:39:01 +0530

    New layout added

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "../lib/useTheme";
 import { CartProvider } from "../lib/cartContext";
-import Footer from "../components/Footer";
+import { FooterNew } from "../components/FooterNew";
+// import { HeaderNew } from "../components/HeaderNew";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <CartProvider>
       <ThemeProvider>
         <div className="min-h-screen bg-white dark:bg-gray-900 dark:text-gray-100 transition-colors">
+          {/* <HeaderNew /> */}
           <Component {...pageProps} />
-          <Footer />
+          <FooterNew />
         </div>
       </ThemeProvider>
     </CartProvider>
